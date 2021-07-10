a660e6d68117b5961f5bca42df6591223d862186
LasithEranga <lasitheranga1@gmail.com>
2021-07-11 04:13:43 +0530

    Start page

---
+++ FMSMob/FMSMob/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using FMSMob.Services;
+using FMSMob.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FMSMob
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ FMSMob/FMSMob/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FMSMob.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FMSMob.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
